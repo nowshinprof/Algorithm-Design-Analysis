7348a57ec77388d2a4db11ec990e9c5b4db46239
Nowshin Fatema <122430484+nowshinprof@users.noreply.github.com>
2023-07-04 14:37:29 +0600

    Update firstclassADAL.py

--- firstclassADAL.py
+++ firstclassADAL.py
@@ -1,33 +1,90 @@
-
-#del k
+#print("Hello Jannat!!")
+#i=9
+#str="hello"
+#print(str)
+#print(i)
+#type(str)
+ 
+#i=9.88
+#k=str(i)
+#inn="hello"
 #print(k)
-type(str)
-
-###tuple
-
+#print(k+inn)
+#type(str)
+ 
+ 
+###TUPLE
+mytup=()   #####tuplee
+mytuple=("apple","orange","mango")
+#print(mytuple[0:3])
+ 
+ 
 x=(6,3)
-y=(2,6)
-    
-if (x<y):
+y=(9,8)
+ 
+if(x>y):
     print("x")
+ 
 else:
-    print("y")
-    
-    ###dictionary
-    dict1= {1:"apple",2:"orange","pear":9}
-
-    print(dict1)
-    print(dict1.items)
-    
-    lst =[]
-    
-    lst.append(1);
-    lst.append(10);
-    lst.append(100);
-    lst.append(1000);
-    lst.append(10000);
-
-print(lst[3])
-    
-    
-    
+    print("y")   
+ 
+ 
+ 
+#####dictionary
+ 
+##map<int,string>mp;  ////for c++
+ 
+dict1={1:"apple",2:"orange","pear":9}  #####dictionary
+ 
+#print(dict1)
+#print(dict1.item())
+ 
+lst=[]   #####array,list
+lst.append(1);
+lst.append(10);
+lst.append(100);
+lst.append(1000);
+lst.append(10000);
+lst.append(100000);
+ 
+for i in range(0,5):
+    print(lst[i])
+ 
+#print(lst[3])
+ 
+ 
+array= [[7,8,9],
+       [7,0,3],
+       [7,2,9]]
+array.append([2,7,55,4])
+ 
+#print(array)
+ 
+ 
+####loop
+ 
+xx=8
+while(xx):
+    #print(xx)
+    if(xx==2):
+      break    
+    xx=xx-1 
+ 
+ 
+lst2=["x","y","z"]
+ 
+for mm in lst2:
+    if mm=="y":
+        continue
+    #print(mm)
+ 
+var2="I study CSE"
+ 
+for ii in var2:
+    print(ii)
+ 
+#print(var2[2:9])
+ 
+print("study" in var2)
+ 
+ 
