dfd7378ff49546898b4be6ef02896b4f1c164526
Nowshin Fatema <122430484+nowshinprof@users.noreply.github.com>
2023-07-04 14:01:40 +0600

    Add files via upload

---
+++ firstclassADAL.py
@@ -0,0 +1,33 @@
+
+#del k
+#print(k)
+type(str)
+
+###tuple
+
+x=(6,3)
+y=(2,6)
+    
+if (x<y):
+    print("x")
+else:
+    print("y")
+    
+    ###dictionary
+    dict1= {1:"apple",2:"orange","pear":9}
+
+    print(dict1)
+    print(dict1.items)
+    
+    lst =[]
+    
+    lst.append(1);
+    lst.append(10);
+    lst.append(100);
+    lst.append(1000);
+    lst.append(10000);
+
+print(lst[3])
+    
+    
+    
